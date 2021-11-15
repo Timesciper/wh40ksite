a05786e108e47ca16aadf2af0a4739ab2c776a7c
Squealer <timesciper@live.ru>
2021-11-15 12:49:48 +0300

    work on secondary

--- wh40game/objectives/models.py
+++ wh40game/objectives/models.py
@@ -8,17 +8,23 @@ class RulesVersion(models.Model):
     short_name = models.CharField(max_length=5, verbose_name='Короткое название')
 
     def __str__(self):
-        return self.name
+        return self.name + ':' + self.short_name
 
     class Meta:
         verbose_name = 'Набор правил'
         verbose_name_plural = 'Наборы правил'
 
+    def save(self, *args, **kwargs):
+        if not self.short_name:
+            self.short_name = self.name[:5]
+
 
 class ObjectiveType(models.Model):
     #  VERSION_CHOICES = [('gt20' , 'Grand Tournament 2021'), ('ow', 'Open War'), ('old', 'Старые миссии')]
+    OBJECTIVE_TYPES = [('prim', 'Primary objective from mission'), ('sec', 'Secondary objective mission')]
     name = models.CharField(max_length=30, verbose_name='Название типа вторичной миссии')
-    version = models.CharField(max_length=20, verbose_name='Версия вторичек')
+    rule_source = models.ForeignKey(RulesVersion, verbose_name='Откуда правила', on_delete=models.CASCADE)
+    #  version = models.CharField(max_length=20, verbose_name='')
 
     def __str__(self):
         return self.name
@@ -27,3 +33,15 @@ class ObjectiveType(models.Model):
         verbose_name = 'Тип вторичной миссии'
         verbose_name_plural = 'Типы вторичной миссии'
 
+
+class SecondaryObjective(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название миссии')
+    rule_source = models.ForeignKey(RulesVersion, on_delete=models.CASCADE, verbose_name='Откуда правила')
+    objective_type = models.ForeignKey(ObjectiveType, on_delete=models.CASCADE, verbose_name='Тип миссии')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        return super(SecondaryObjective, self).save(*args, **kwargs)
